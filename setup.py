7d37ab9d720edcc20d9056102bce56d61ee4f301
Pascal Bouchareine <kalou@tfz.net>
2023-11-24 12:56:22 -0800

    lint setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
-setup(entry_points = {
-        'console_scripts': ['plants_server=plants.server:server'],
+setup(
+    entry_points={
+        "console_scripts": ["plants_server=plants.server:server"],
     },
-    install_requires = [
-        'Flask',
-        'gpiozero',
-        'prometheus-client',
-        'PyYAML',
-        'smbus',
+    install_requires=[
+        "Flask",
+        "gpiozero",
+        "prometheus-client",
+        "PyYAML",
+        "smbus",
     ],
-    name='plants'
+    name="plants",
 )
